684fea50dd47d0fa23099cfeed2c73beafd596b9
eypbng508-spec <eypbng508@gmail.com>
2026-05-26 00:33:12 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,13 @@
+// data.js
+window.kpssData = {
+    "Tarih": {
+        "İslamiyet Öncesi Türk Tarihi": window.tarih_1_sorulari || [],
+        "Türk-İslam Tarihi": window.tarih_2_sorulari || [] 
+    },
+    "Coğrafya": {
+        "Türkiye'nin Coğrafi Konumu": window.cografya_1_sorulari || []
+    },
+    "Vatandaşlık": {
+        "Hukukun Temel Kavramları": window.vatandaslik_1_sorulari || []
+    }
+};
